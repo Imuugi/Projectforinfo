9388a5ea25bb2fe433a1c3fec03b1906671d60a1
NaveedJM <naveed.mohammed1@my.uwi.edu>
2025-04-20 23:46:17 +0000

    fixes cc added back buttons and logout

--- App/views/auth.py
+++ App/views/auth.py
@@ -67,7 +67,7 @@ def login_action():
 
 @auth_views.route('/logout', methods=['GET'])
 def logout_action():
-    response = redirect(request.referrer) 
+    response = redirect(url_for('index_views.show_login'))
     flash("Logged Out!")
     unset_jwt_cookies(response)
     return response
