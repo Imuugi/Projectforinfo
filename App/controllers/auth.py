4335e58e56576e4dce7733c39717aaeb7b29a795
NaveedJM <naveed.mohammed1@my.uwi.edu>
2025-04-20 06:44:59 +0000

    NAME WORKS

--- App/controllers/auth.py
+++ App/controllers/auth.py
@@ -3,29 +3,28 @@ from flask_jwt_extended import create_access_token, jwt_required, JWTManager, ge
 from App.models import User
 
 def login(username, password):
-  user = User.query.filter_by(username=username).first()
-  if user and user.check_password(password):
-    return create_access_token(identity=username)
-  return None
+    user = User.query.filter_by(username=username).first()
+    if user and user.check_password(password):
+        return create_access_token(identity=username)
+    return None
 
 
 def setup_jwt(app):
-  jwt = JWTManager(app)
+    jwt = JWTManager(app)
 
-  # configure's flask jwt to resolve get_current_identity() to the corresponding user's ID
-  @jwt.user_identity_loader
-  def user_identity_lookup(user_id):
-    user = User.query.filter_by(id = user_id).one_or_none()
-    if user:
-        return user.id
-    return None
+    # Configures flask-jwt to resolve get_current_identity() to the corresponding user's ID
+    @jwt.user_identity_loader
+    def user_identity_lookup(username):
+      return username
 
-  @jwt.user_lookup_loader
-  def user_lookup_callback(_jwt_header, jwt_data):
-    identity = jwt_data["sub"]
-    return User.query.get(identity)
 
-  return jwt
+    @jwt.user_lookup_loader
+    def user_lookup_callback(_jwt_header, jwt_data):
+        identity = jwt_data["sub"]
+        return User.query.filter_by(username=identity).first()
+
+    return jwt
+
 
 
 # Context processor to make 'is_authenticated' available to all templates
@@ -34,13 +33,17 @@ def add_auth_context(app):
   def inject_user():
       try:
           verify_jwt_in_request()
-          user_id = get_jwt_identity()
-          current_user = User.query.get(user_id)
-          is_authenticated = True
+          username = get_jwt_identity()
+          if username:
+              current_user = User.query.filter_by(username=username).first()
+              is_authenticated = current_user is not None
+          else:
+              current_user = None
+              is_authenticated = False
       except Exception as e:
           print(e)
-          is_authenticated = False
           current_user = None
+          is_authenticated = False
       return dict(is_authenticated=is_authenticated, current_user=current_user)
 
 @jwt_required()
