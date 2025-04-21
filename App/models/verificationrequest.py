17df465ddd2836f97a4ebc9d6b8e1b45b64ac4d6
NaveedJM <naveed.mohammed1@my.uwi.edu>
2025-04-21 05:04:10 +0000

    shows username

--- App/models/verificationrequest.py
+++ App/models/verificationrequest.py
@@ -1,11 +1,15 @@
 from App.database import db
 from datetime import datetime
+from App.models import Tenant 
+
 class VerificationRequest(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     tenant_id = db.Column(db.Integer, db.ForeignKey('tenants.id'), nullable=False)
     landlord_id = db.Column(db.Integer, db.ForeignKey('landlords.id'), nullable=False)
     apartment_id = db.Column(db.Integer, db.ForeignKey('apartment_listings.id'), nullable=False)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
+    tenant = db.relationship('Tenant', backref='verification_requests')
 
     def __init__(self,tenant_id,landlord_id,apartment_id):
         self.tenant_id = tenant_id
